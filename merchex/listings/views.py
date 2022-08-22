ee59c2eb57afe48724c9c1e7d753ab8e0452c7a3
MKH-Beldi <mohamadkhalil.beldi@esprit.tn>
2022-08-22 22:58:52 +0100

    add views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
 
+def about(request: HttpRequest):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+def listings(request):
+    return HttpResponse('<h1>List of announcements for articles</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Contact US</h1>')
