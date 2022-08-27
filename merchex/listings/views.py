5e378fb88f4b30174c9aa556b5588e49bd008027
MKH-Beldi <mohamadkhalil.beldi@esprit.tn>
2022-08-27 10:24:15 +0100

    display list & detail

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,19 +4,32 @@ from django.shortcuts import render
 
 from listings.models import *
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
     return render(request,
-                  'listings/hello.html',
+                  'listings/band_list.html',
                   context={'bands': bands}
                   )
-def listings(request):
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request,
+                  'listings/band_detail.html',
+                  context={'band': band}
+                  )
+def listing_list(request):
     listings = Listings.objects.all()
     return render(request,
-                  'listings/listings.html',
+                  'listings/listings_list.html',
                   context={'listings': listings}
                   )
 
+def listing_detail(request, id):
+    listing = Listings.objects.get(id=id)
+    return render(request,
+                  'listings/listings_detail.html',
+                  context={'listing': listing}
+                  )
 def about(request: HttpRequest):
     return render(request,
                   'listings/about_us.html',
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -19,8 +19,10 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-    path('about-us/', views.about),
-    path('listings/', views.listings),
+    path('bands/', views.band_list, name='band_list'),
+    path('bands/<int:id>/', views.band_detail, name='band_detail'),
+    path('listings/', views.listing_list, name='listing_list'),
+    path('listings/<int:id>/', views.listing_detail, name='listing_detail'),
     path('contact-us/', views.contact),
+    path('about-us/', views.about),
 ]
